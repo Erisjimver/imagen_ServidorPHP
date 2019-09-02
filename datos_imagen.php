4cf9277faf87f599324a9c41189c2db57243f06c
erisjimver <erisjinver@gmail.com>
2019-09-02 12:10:48 -0500

    subiendo imagenes a tablas

--- datos_imagen.php
+++ datos_imagen.php
@@ -6,20 +6,55 @@
  $tamaño_imagen=$_FILES["imagen"]["size"];
 
  //echo $tamaño_imagen;
-
+ //echo $tipo_imagen;
 
 
  if ($tamaño_imagen<=1000000) {
 
-	 //ruta de la carpeta destino en el servidor
+ 	if ($tipo_imagen=="image/jpg"||$tipo_imagen=="image/png"||$tipo_imagen=="image/jpeg") {
+ 		 //ruta de la carpeta destino en el servidor
 	 $carpeta_destino=$_SERVER["DOCUMENT_ROOT"] . "/intranet/uploads/";
 
 	//movemos la imagen del directorio temporal o carpeta temporal al directorio o carpeta escogida
 	 move_uploaded_file($_FILES["imagen"]["tmp_name"],$carpeta_destino . $nombre_imagen); 	
-	 echo "Se subio";
+	 //echo "Se subio";
+ 	}
+ 	else{
+ 		echo "No es un formato permitido";
+ 	}
+	
 	 }
 	 else{
 	 	echo "El tamaño de la imagen es demaciado grande....";
 	 }
+
+
+	require("datos_conexion.php");
+
+	$conexion=mysqli_connect($db_host,$db_usuario,$db_contra);
+
+	if(mysqli_connect_errno()){
+		echo "Error de  conexion a bd";
+		exit();
+	}
+	//else{
+
+	mysqli_select_db($conexion,$db_nombre) or die("No se encuentra la base de datos");
+
+	mysqli_set_charset($conexion,"utf8");//linea que permite mostrar 
+
+	$sql="update productos set foto='$nombre_imagen' where códigoartículo='AR01'";
+	
+	$resultado=mysqli_query($conexion,$sql);	
+
+
+
+
+
+
+
+
+
+
 
 ?>	
