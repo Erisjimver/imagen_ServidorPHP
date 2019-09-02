c58abd6bcc1d8c722c61af199770aad60b205f12
erisjimver <erisjinver@gmail.com>
2019-09-02 09:39:47 -0500

    se limita el tamaño del archivo a subir, ademas de reconocer que se puede subir cualquier archivo

--- datos_imagen.php
+++ datos_imagen.php
@@ -5,9 +5,21 @@
  $tipo_imagen=$_FILES["imagen"]["type"];
  $tamaño_imagen=$_FILES["imagen"]["size"];
 
- //ruta de la carpeta destino en el servidor
- $carpeta_destino=$_SERVER["DOCUMENT_ROOT"] . "/intranet/uploads/";
+ //echo $tamaño_imagen;
 
-//movemos la imagen del directorio temporal o carpeta temporal al directorio o carpeta escogida
- move_uploaded_file($_FILES["imagen"]["tmp_name"],$carpeta_destino . $nombre_imagen);
+
+
+ if ($tamaño_imagen<=1000000) {
+
+	 //ruta de la carpeta destino en el servidor
+	 $carpeta_destino=$_SERVER["DOCUMENT_ROOT"] . "/intranet/uploads/";
+
+	//movemos la imagen del directorio temporal o carpeta temporal al directorio o carpeta escogida
+	 move_uploaded_file($_FILES["imagen"]["tmp_name"],$carpeta_destino . $nombre_imagen); 	
+	 echo "Se subio";
+	 }
+	 else{
+	 	echo "El tamaño de la imagen es demaciado grande....";
+	 }
+
 ?>	
